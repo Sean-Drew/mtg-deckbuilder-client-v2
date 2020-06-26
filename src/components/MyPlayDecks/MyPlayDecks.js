efc17923c8cb635f1b5054d916d52f97ff3c9b2e
Sean-Drew <SDrew989@gmail.com>
2020-06-26 18:06:00 -0400

    X

--- src/components/MyPlayDecks/MyPlayDecks.js
+++ src/components/MyPlayDecks/MyPlayDecks.js
@@ -5,7 +5,7 @@ import apiUrl from '../../apiConfig'
 import axios from 'axios'
 
 // import design & styling items
-import 'antd/dist/antd.css'
+
 // import MtgCardBack from '../../Images/mtg-card-back-2.jpg' /* Possible option for 'deck' image */
 import MtgLogo1 from '../../Images/mtg-logo-1.jpg' /* Possible option for 'deck' image */
 
